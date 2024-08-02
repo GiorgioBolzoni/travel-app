824e65f09f4db030268d90328ab25875bde02ea0
GiorgioBolzoni <139789755+GiorgioBolzoni@users.noreply.github.com>
2024-08-02 10:47:06 +0200

    creata base main per mappa

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
 
 <div class="container">
   <Header></Header>
+
+  <Main></Main>
 </div>
 
 </template>
 
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
 
 export default {
     components:{
       Header,
+      Main,
     },
     data(){
       return{
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,49 @@
 <template>
-  
+    <section id="filter">
+        <div class="d-flex justify-content-center">
+            <select name="travel" id="travel"  class="form-select">
+                <option 
+                
+                value="all">All</option>
+            </select>
+        </div>
+    </section>
+
+    <section id="travel-map" class="py-4">
+        <div class="card p-4">
+            travel
+        </div>
+    </section>
 </template>
 
 
 <script>
-
+    export default {
+        name: 'Main',
+        data() {
+            return{
+                menu: [
+                    {name: 'All', value:'All'},
+                    {name: 'Italy', value:'Italy'},
+                    {name: 'Scotland', value:'Scotland'},
+                    {name: 'Thailand', value:'Thailand'},
+                ],
+            }
+        }
+    }
 </script>
 
 
 <style lang="scss" scoped>
-
+    select{
+        width: 100px;
+        &:hover{
+            cursor: pointer;
+        };
+        option{
+            &:hover{
+                cursor: pointer !important;
+            }
+        }
+    }
 </style>
